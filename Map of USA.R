4e46476148235e3a6045425aa6220f826e4e0c71
Fernando Mestre <67239359+mestrefernando@users.noreply.github.com>
2020-12-19 19:16:10 +0000

    Add files via upload

--- Map of USA.R
+++ Map of USA.R
@@ -12,4 +12,10 @@ usa <- map_data("usa")
 ggplot() + geom_polygon(data = usa, aes(x=long, y = lat, group = group)) + 
   coord_fixed(1.3)
 
+states <- map_data("state")
+dim(states)
 
+ggplot(data = states) + 
+  geom_polygon(aes(x = long, y = lat, fill = region, group = group), color = "white") + 
+  coord_fixed(1.3) +
+  guides(fill=FALSE)
